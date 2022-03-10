fb8521d0b7510afddbe0eab81df87cca3aebe0b0
jtsang4 <wtzeng1@gmail.com>
2022-03-10 13:30:37 +0800

    chore: add apps

--- apps/app-b/src/main.tsx
+++ apps/app-b/src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div>test page b</div>
   </React.StrictMode>,
   document.getElementById('root')
 )
