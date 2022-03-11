a8a5d6fdead9b9137e3a02bc682a5e89d793e318
jtsang4 <wtzeng1@gmail.com>
2022-03-11 15:05:45 +0800

    feat: optimize proxy script to support non-https

--- scripts/proxy.js
+++ scripts/proxy.js
@@ -1,29 +1,38 @@
+const DEV_HOSTNAME = 'localhost'
+
 // TODO: 根据真实业务规则修改代理端口
-function getPort() {
-  if (window.location.pathname.startsWith('/foo')) {
-    return '4001'
+function getPort(https) {
+  // https 的走反代后的端口
+  if (https) {
+    if (window.location.pathname.startsWith('/foo')) {
+      return '4001'
+    }
+    return '4002'
   }
-  return '4002'
+  if (window.location.pathname.startsWith('/foo')) {
+    return '3001'
+  }
+  return '3002'
 }
 
 function getOrigin(https) {
-  return `http${https ? 's' : ''}://localhost:${getPort()}`
+  return `http${https ? 's' : ''}://${DEV_HOSTNAME}:${getPort(https)}`
 }
 
-function setupDevPort() {
-  window.__DEV_HMR_HOSTNAME = 'localhost'
-  window.__DEV_HMR_PORT__ = getPort()
+function setupDevPort(https) {
+  window.__DEV_HMR_HOSTNAME = DEV_HOSTNAME
+  window.__DEV_HMR_PORT__ = getPort(https)
 }
 
-function appendViteScript() {
+function appendViteScript(https) {
   const viteClientScript = document.createElement('script')
   viteClientScript.type = 'module'
-  viteClientScript.src = `${getOrigin(true)}/@vite/client`
+  viteClientScript.src = `${getOrigin(https)}/@vite/client`
 
   const refreshRuntimeScript = document.createElement('script')
   refreshRuntimeScript.type = 'module'
   refreshRuntimeScript.textContent = `
-  import RefreshRuntime from "${getOrigin(true)}/@react-refresh"
+  import RefreshRuntime from "${getOrigin(https)}/@react-refresh"
   RefreshRuntime.injectIntoGlobalHook(window)
   window.$RefreshReg$ = () => {}
   window.$RefreshSig$ = () => (type) => type
@@ -34,13 +43,13 @@ function appendViteScript() {
   document.head.appendChild(refreshRuntimeScript)
 }
 
-function appendEntry() {
+function appendEntry(https) {
   const entryScript = document.createElement('script')
   entryScript.type = 'module'
-  entryScript.src = `${getOrigin(true)}/src/main.tsx`
+  entryScript.src = `${getOrigin(https)}/src/main.tsx`
   document.body.appendChild(entryScript)
 }
 
-setupDevPort()
-appendViteScript()
-appendEntry()
+setupDevPort(true)
+appendViteScript(true)
+appendEntry(true)
